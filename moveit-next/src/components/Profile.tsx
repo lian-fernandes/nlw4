cdb7f366c66160b455d46a9c9fd112b8600186cf
Lian Araújo Fernandes <liansutil@gmail.com>
2021-02-23 14:39:17 -0300

    next.js migration

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://www.treehugger.com/thmb/Vl5IAxypuNR_igpm7Uhl7q2-ks8=/2136x1403/filters:fill(auto,1)/yellow-mongoose-portrait-01-4873d2399cb54ea48448b0bda37bda1e.jpg" alt="Pepega"/>
+            <div>
+                <strong>Bixin</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 99
+                </p>
+            </div>
+        </div>
+    );
+}
